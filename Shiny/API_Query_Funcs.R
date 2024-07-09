d8811f245352d8504fa45f3eded17d199b2f0f61
Charlie Armentrout <chtrout@cox.net>
2024-07-09 12:45:39 -0400

    deleted playerCount function from previous commit as it was accidentally left it. Now just added 4 more functions of the same attributes, but looking at advanced stats during playoffs which would be interesting to be compared to regular season performance and see who really performs when the lights are the brightest

--- Shiny/API_Query_Funcs.R
+++ Shiny/API_Query_Funcs.R
@@ -25,7 +25,6 @@ query_params <- list(
 
 # Get player data using query parameters
 player_data_query <- get_player_data_advanced_query(query_params)
-player_data_query <- as_tibble(player_data_query)
 print(player_data_query)
 
 
@@ -33,9 +32,6 @@ print(player_data_query)
 
 get_player_data_advanced_by_name <- function(player_name) {
   base_url <- paste0("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvanced/name/", URLencode(player_name))
-  
-  # Print the request URL for debugging
-  cat("Request URL:", base_url, "\n")
   
   response <- httr::GET(url = base_url)
   stop_for_status(response)
@@ -73,9 +69,6 @@ print(player_data_by_season)
 get_player_data_advanced_by_team <- function(team_name) {
   base_url <- paste0("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvanced/team/", URLencode(team_name))
   
-  # Print the request URL for debugging
-  cat("Request URL:", base_url, "\n")
-  
   response <- httr::GET(url = base_url)
   stop_for_status(response)
   
@@ -91,18 +84,89 @@ print(player_data_by_team)
 
 
 
-get_player_data_advanced_count <- function() {
-  base_url <- "http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvanced/count"
+
+
+
+
+#Advanced Stats During Playoff Functions
+get_player_data_advancedplayoff_query <- function(query_params) {
+  base_url <- "http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvancedPlayoffs/query"
+  
+  response <- httr::GET(url = base_url, query = query_params)
+  stop_for_status(response)
+  
+  player_data <- content(response, as = "text")
+  player_data <- fromJSON(player_data)
+  
+  return(player_data)
+}
+
+# Example usage:
+# Define query parameters
+query_params <- list(
+  playerName = "LeBron James",
+  sortBy = "PlayerName",
+  ascending = TRUE,
+  pageNumber = 1,
+  pageSize = 30
+)
+
+# Get player data using query parameters
+player_data_query <- get_player_data_advancedplayoff_query(query_params)
+print(player_data_query)
+
+
+
+
+get_player_data_advancedplayoff_by_name <- function(player_name) {
+  base_url <- paste0("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvancedPlayoffs/name/", URLencode(player_name))
   
   response <- httr::GET(url = base_url)
   stop_for_status(response)
   
-  player_count <- content(response, as = "text")
-  player_count <- fromJSON(player_count)
+  player_data <- content(response, as = "text")
+  player_data <- fromJSON(player_data)
   
-  return(player_count)
+  return(player_data)
 }
 
 # Example usage:
-player_count <- get_player_data_advanced_count()
-print(player_count)
+player_data_by_name <- get_player_data_advancedplayoff_by_name("LeBron James")
+print(player_data_by_name)
+
+
+
+get_player_data_advancedplayoff_by_season <- function(season) {
+  base_url <- paste("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvancedPlayoffs/season", season, sep = "/")
+  
+  response <- httr::GET(url = base_url)
+  stop_for_status(response)
+  
+  player_data <- content(response, as = "text")
+  player_data <- fromJSON(player_data)
+  
+  return(player_data)
+}
+
+# Example usage:
+player_data_by_season <- get_player_data_advancedplayoff_by_season("2023")
+print(player_data_by_season)
+
+
+
+get_player_data_advancedplayoff_by_team <- function(team_name) {
+  base_url <- paste0("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataAdvancedPlayoffs/team/", URLencode(team_name))
+  
+  response <- httr::GET(url = base_url)
+  stop_for_status(response)
+  
+  player_data <- content(response, as = "text")
+  player_data <- fromJSON(player_data)
+  
+  return(player_data)
+}
+
+# Example usage:
+player_data_by_team <- get_player_data_advancedplayoff_by_team("LAL")
+print(player_data_by_team)
+
